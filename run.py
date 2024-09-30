2efc38601bdf8637d4cd76a8d37d8589ae7694ea
Achuthantm <47918721+Achuthantm@users.noreply.github.com>
2024-09-30 20:08:34 +0530

    Updated to enable chunking

--- run.py
+++ run.py
@@ -1,14 +1,9 @@
 import os   
 import shutil 
-import requests
-
-def download_file(url, destination):
-    response = requests.get(url)
-    with open(destination, 'wb') as file:
-        file.write(response.content)
+import wget
 
 if not os.path.exists('./GFPGANv1.3.pth'):
-    download_file('https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth', './GFPGANv1.3.pth')
+    wget.download('https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth', './GFPGANv1.3.pth')
 
 try:
     shutil.rmtree('./inputs')
